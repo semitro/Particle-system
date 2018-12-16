e7684143ab757e227ceb7975bd75ae3675292bcd
semitro <semitro_8@mail.ru>
2018-12-16 11:57:59 +0300

    Hello sfml

--- SarticlePystem.pro
+++ SarticlePystem.pro
@@ -5,3 +5,8 @@ CONFIG -= qt
 
 SOURCES += \
         main.cpp
+
+INCLUDEPATH += "/usr/include/SFML/"
+
+CONFIG(release, debug|release): LIBS +=  -lsfml-audio -lsfml-graphics -lsfml-network -lsfml-window -lsfml-system
+CONFIG(debug, debug|release): LIBS += -lsfml-audio -lsfml-graphics -lsfml-network -lsfml-window -lsfml-system
--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
-#include <iostream>
-
-using namespace std;
-
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 int main()
 {
-	cout << "Hello World!" << endl;
-	return 0;
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	// Load a sprite to display
+	// Create a graphical text to display
+	sf::Text text;
+	// Load a music to play
+	// Play the music
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// Clear screen
+		window.clear();
+		// Draw the sprite
+		// Draw the string
+		window.draw(text);
+		// Update the window
+		window.display();
+	}
+	return EXIT_SUCCESS;
 }
